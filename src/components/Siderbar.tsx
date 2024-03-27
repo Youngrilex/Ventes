d61fc60d5439ad9941c091bd03bfd5334310b377
Youngrilex <ridwanakindele6@gmail.com>
2024-03-27 14:00:30 +0100

    product

--- src/components/Siderbar.tsx
+++ src/components/Siderbar.tsx
@@ -6,7 +6,7 @@ import { BsBookmarkDashFill, BsFileTextFill } from "react-icons/bs";
 const Siderbar = () => {
   return (
     <aside className="bg-[#5041BC] rounded-3xl h-full w-[20vw] absolute top-0 left-0">
-        <img className="w-32 h-12 m-8" src="../assets/VENTES.png" alt="" />
+        <img className="w-32 h-12 m-8" src="/assets/VENTES.png" alt="" />
         <div className="w-36 h-[1px] m-8 bg-[#8A7DD0]" />
         <div className="mr-16 m-6 mt-8 flex flex-col text-white text-[15px] font-semibold">
           <div className="hover:bg-white hover:text-[#5041BC] px-4 py-2 hover:rounded-xl flex items-center gap-6">
@@ -41,7 +41,7 @@ const Siderbar = () => {
           </div>
         </div>
         <div className="rounded-2xl bg-[#ffffff] mx-6 mr-16 flex flex-col items-center">
-          <img className="w-36 h-28 m-2" src="/src/assets/icon.png" alt="" />
+          <img className="w-36 h-28 m-2" src="/assets/icon.png" alt="" />
           <div className="font-semibold text-[14px]">Want to upgrade</div>
           <div className="rounded-full bg-[#FF8057] font-semibold text-[#ffffff] m-2 text-[14px] px-4 text-center">Upgrade now</div>
         </div>
